22ae28b9ccd8acb30be27cfb48de4edd6b48d8bd
Naitik <nkotharin558@gmail.com>
2025-06-11 10:33:31 -0400

    added env variables

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBPDqmy6VlnTf8YrRoM2tEJIdDu2vYIEEs",
-  authDomain: "ru-food.firebaseapp.com",
-  projectId: "ru-food",
-  storageBucket: "ru-food.firebasestorage.app",
-  messagingSenderId: "1007983570328",
-  appId: "1:1007983570328:web:b3a74faa4d6676c293cfed",
-  measurementId: "G-PCGHEKPSHK"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
